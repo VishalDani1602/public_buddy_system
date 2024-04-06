c1bce3563cca6773623e454f84194b2b2c711a45
Vishal Dani <vishal.dani123@gmail.com>
2024-04-05 22:31:37 -0700

    initial commit

---
+++ src/main/java/com/example/publicbuddysystem/PublicbuddysystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.publicbuddysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicbuddysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicbuddysystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/publicbuddysystem/PublicbuddysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicbuddysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicbuddysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
