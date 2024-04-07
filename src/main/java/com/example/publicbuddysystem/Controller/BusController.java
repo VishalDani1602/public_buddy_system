2ead6ae5131054ba9bf2c866d0ad8d172ff80e37
Vishal Dani <vishal.dani123@gmail.com>
2024-04-07 00:28:59 -0700

    added busMap endpoint for map

--- src/main/java/com/example/publicbuddysystem/Controller/BusController.java
+++ src/main/java/com/example/publicbuddysystem/Controller/BusController.java
@@ -3,9 +3,14 @@ package com.example.publicbuddysystem.Controller;
 import com.example.publicbuddysystem.Model.Bus;
 import com.example.publicbuddysystem.Service.BusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -25,6 +30,22 @@ public class BusController {
     public ResponseEntity<List<Bus>> getAllBuses() {
         List<Bus> buses = busService.getAllBuses();
         return ResponseEntity.ok(buses);
+    }
+
+    @GetMapping("/busmap")
+    public ResponseEntity<String> getBusMap() {
+        try {
+            // Read content of buses.json file
+            ClassPathResource resource = new ClassPathResource("busData.json");
+            byte[] bytes = Files.readAllBytes(Paths.get(resource.getURI()));
+            String jsonData = new String(bytes);
+
+            // Return JSON response
+            return ResponseEntity.ok(jsonData);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading JSON file");
+        }
     }
 
     // Endpoint to retrieve a specific bus by its busNumber
